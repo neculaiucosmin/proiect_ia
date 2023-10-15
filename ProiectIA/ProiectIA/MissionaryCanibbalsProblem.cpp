dcb51ac56293cd8177286edf60aa128ca17cd62e
cosmin neculaiu <cosminflorinel23@gmail.com>
2023-10-15 23:35:05 +0300

    Created the boat for entity transfer

---
+++ ProiectIA/ProiectIA/MissionaryCanibbalsProblem.cpp
@@ -0,0 +1,56 @@
+﻿#include "MissionaryCanibbalsProblem.h"
+
+
+/// \brief
+/// Initializeaza malul stang cu 3 canibali, respectiv 3 misionari
+void MissionaryCanibbalsProblem::initProblem()
+{
+    if (!isInit)
+    {
+        
+        leftShore->canibali.push_back(new cannibal);
+        leftShore->canibali.push_back(new cannibal);
+        leftShore->canibali.push_back(new cannibal);
+
+        leftShore->misionari.push_back(new missionary);
+        leftShore->misionari.push_back(new missionary);
+        leftShore->misionari.push_back(new missionary);
+
+        isInit = true;
+    }
+    else
+    {
+        std::cout << "Programul este deja initializat" << std::endl;
+    }
+}
+
+/// \brief Verifica numarul entitatilor de pe fiecare mal
+/// \return true daca numaralul misionarilor este mai mare sau egal decat numarul canibalilor
+/// \return false daca numarul canibalilor este mai mare
+bool MissionaryCanibbalsProblem::checkCannibalsNumber()
+{
+    auto nrCanibaliStanga = leftShore->canibali.size();
+    auto nrMisionariStanga = leftShore->misionari.size();
+
+    auto nrCanibaliDreapta = rightShore->canibali.size();
+    auto nrMisionariDreapta = rightShore->misionari.size();
+
+    if (nrCanibaliStanga > nrMisionariStanga)
+    {
+        return false;
+    }
+
+    if (nrCanibaliDreapta > nrMisionariDreapta)
+    {
+        return false;
+    }
+
+
+    return true;
+}
+
+MissionaryCanibbalsProblem::~MissionaryCanibbalsProblem()
+{
+    delete leftShore;
+    delete rightShore;
+}
---
+++ ProiectIA/ProiectIA/MissionaryCanibbalsProblem.h
@@ -0,0 +1,119 @@
+﻿#pragma once
+#include <vector>
+#include <iostream>
+
+enum EntityTypeAction
+{
+    MISSIONARY,
+    MISSIONARY2,
+    CANNIBAL,
+    CANNIBAL2,
+    
+};
+
+struct missionary
+{
+};
+
+struct cannibal
+{
+};
+
+struct shore
+{
+    std::vector<missionary*> misionari;
+    std::vector<cannibal*> canibali;
+};
+
+struct barca
+{
+    cannibal* cannibal1 = nullptr;
+    cannibal* cannibal2 = nullptr;
+
+    missionary* missionary1 = nullptr;
+    missionary* missionary2 = nullptr;
+
+    void load(missionary* m1 = nullptr, missionary* m2 = nullptr, cannibal* c1 = nullptr, cannibal* c2 = nullptr)
+    {
+        if (m1 != nullptr)
+        {
+            missionary1 = m1;
+        }
+
+        if (m2 != nullptr)
+        {
+            missionary2 = m2;
+        }
+
+        if (c1 != nullptr)
+        {
+            cannibal1 = c1;
+        }
+
+        if (c2 != nullptr)
+        {
+            cannibal2 = c2;
+        }
+    }
+
+    /// \brief Allow unloading the item from the boat
+    /// \param _shore A reference to the shore where you want to unload the entities
+    /// \param entityToMove Tell which entity to move from the boat.
+    void unload(shore& _shore, EntityTypeAction entityToMove)
+    {
+        switch(entityToMove)
+        {
+        case CANNIBAL:
+            if (cannibal1 != nullptr)
+            {
+                _shore.canibali.push_back(cannibal1);
+                cannibal1 = nullptr;
+            }
+            else
+                std::cout << "Nu exista niciun canibal in barca";
+            break;
+        case CANNIBAL2:
+            if (cannibal2 != nullptr)
+            {
+                _shore.canibali.push_back(cannibal2);
+                cannibal2 = nullptr;
+            }
+            else
+                std::cout << "Nu exista al 2-lea canibal in barca";
+            break;
+    
+        case MISSIONARY:
+            if (missionary1 != nullptr)
+            {
+                _shore.misionari.push_back(missionary1);
+                missionary1 = nullptr;
+            }
+            else
+                std::cout << "Nu exista misionari in barca";
+            break;
+        case MISSIONARY2:
+            if (missionary2 != nullptr)
+            {
+                _shore.misionari.push_back(missionary2);
+                missionary2 = nullptr;
+            }
+            else
+                std::cout << "Nu exista al 2-lea misionar in barca";
+            break;
+        }
+    }
+};
+
+class MissionaryCanibbalsProblem
+{
+private:
+    bool isInit = false;
+    bool checkCannibalsNumber();
+
+public:
+    ~MissionaryCanibbalsProblem();
+    shore* leftShore = new shore;
+    shore* rightShore = new shore;
+
+    void initProblem();
+};
